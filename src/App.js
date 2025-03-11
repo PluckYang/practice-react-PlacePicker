385e8bb4673b28714901f0f7bfdca72af8faa2f5
Pluck Yang <pluck.yang@gmail.com>
2025-03-11 20:28:38 +0900

    useEffect‘s cleanup function：another exaxmple

--- src/App.js
+++ src/App.js
@@ -1,104 +1,46 @@
-import { useRef, useState, useEffect, useCallback } from "react";
-import Places from "./components/Places";
-import { AVAILABLE_PLACES } from "./data";
-import Modal from "./components/Modal";
-import DeleteConfirmation from "./components/DeleteConfirmation";
-import logoImg from "./assets/logo.png";
-import { sortPlacesByDistance } from "./loc.js";
+import { useEffect, useState } from "react";
 
-const storedIds = JSON.parse(localStorage.getItem("selectedPlace")) || [];
-const storedPlaces = storedIds.map((id) =>
-  AVAILABLE_PLACES.find((Place) => Place.id === id)
-);
+const TIMER = 3000;
 
-function App() {
-  const selectedPlace = useRef();
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [availablePlacce, setAvailablePlacce] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const sortedPlaces = sortPlacesByDistance(
-        AVAILABLE_PLACES,
-        position.coords.latitude,
-        position.coords.longitude
-      );
+    const interval = setInterval(() => {
+      console.log("INTERVAL");
+      setRemainingTime((preTime) => preTime - 10);
+    }, 10);
 
-      setAvailablePlacce(sortedPlaces);
-    });
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
-  function handleStartRemovePlace(id) {
-    setModalIsOpen(true);
-    selectedPlace.current = id;
-  }
+  useEffect(() => {
+    console.log("TIMER SET");
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, TIMER);
 
-  function handleStopRemovePlace() {
-    setModalIsOpen(false);
-  }
-
-  function handleSelectPlace(id) {
-    setPickedPlaces((prevPickedPlaces) => {
-      if (prevPickedPlaces.some((place) => place.id === id)) {
-        return prevPickedPlaces;
-      }
-      const place = AVAILABLE_PLACES.find((place) => place.id === id);
-      return [place, ...prevPickedPlaces];
-    });
-
-    const storedIds = JSON.parse(localStorage.getItem("selectedPlace")) || [];
-    if (storedIds.indexOf(id) === -1) {
-      localStorage.setItem("selectedPlace", JSON.stringify([id, ...storedIds]));
-    }
-  }
-
-  const handleRemovePlace = useCallback(function handleRemovePlace() {
-    setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
-    );
-    setModalIsOpen(false);
-
-    const storedIds = JSON.parse(localStorage.getItem("selectedPlace")) || [];
-    localStorage.setItem(
-      "selectedPlace",
-      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
-    );
+    return () => {
+      console.log("Cleaning up timer!");
+      clearTimeout(timer);
+    };
   }, []);
 
   return (
-    <>
-      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
-      </Modal>
-
-      <header>
-        <img src={logoImg} alt="Stylized globe" />
-        <h1>PlacePicker</h1>
-        <p>
-          Create your personal collection of places you would like to visit or
-          you have visited.
-        </p>
-      </header>
-      <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText={"Select the places you would like to visit below."}
-          places={pickedPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
-        <Places
-          title="Available Places"
-          places={availablePlacce}
-          fallbackText="Sorting places by distance..."
-          onSelectPlace={handleSelectPlace}
-        />
-      </main>
-    </>
+    <div id="delete-confirmation">
+      <h2>Are you sure?</h2>
+      <p>Do you really want to remove this place?</p>
+      <div id="confirmation-actions">
+        <button onClick={onCancel} className="button-text">
+          No
+        </button>
+        <button onClick={onConfirm} className="button">
+          Yes
+        </button>
+      </div>
+      <progress value={remainingTime} max={TIMER} />
+    </div>
   );
 }
-
-export default App;
